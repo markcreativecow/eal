9bb8cdbcff7677fd767d90ac2888dd66db2c35a5
Mark Ascroft <mark@hownowcreativecow.co.uk>
2014-01-16 14:18:19 +0000

    Auto scroll to event results

--- js/index.js
+++ js/index.js
@@ -36,6 +36,7 @@ function generateCalendar(events) {
         $('.custom-content').remove();
     }
     function showEvents(content) {
+		alert(content.html());
         hideEvents();
         $('#busy').show();
         setTimeout(function(){
