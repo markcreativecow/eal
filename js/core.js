ecf0cb70009d2ac41f14cd58e3f8f0b2e04c9c45
Mark Ascroft <mark@hownowcreativecow.co.uk>
2014-02-10 15:36:14 +0000

    Updating open in browser links

--- js/core.js
+++ js/core.js
@@ -23,6 +23,7 @@ $(document).ajaxError(function(event, request, settings) {
 function openURL(urlString){
     myURL = encodeURI(urlString);
     window.open(myURL, '_blank');
+	return false;
 }
 function hideOverlays() {
     $('.overlay').hide();
